d449ba19691ddcf5d5d9240728a271888b19e0d8
danic <dalbacarrasco@gmail.com>
2023-02-12 16:53:40 -0400

    saludo inicial y giroscopio

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_tts/flutter_tts.dart';
+import 'package:sensors_plus/sensors_plus.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,104 +15,64 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Ayudame'),
+        ),
+        body: Center(
+          child: MyHomePage(),
+        )
+      ),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class MyHomePage extends StatelessWidget {
+  final FlutterTts flutterTts = FlutterTts();
+  MyHomePage({Key? key}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  speak(String text) async {
+  await flutterTts.setLanguage("es-ES");
+  await flutterTts.setPitch(1);
+  await flutterTts.speak(text);
   }
+
+  double dx = 200, dy = 200;
+  double dx1 = 200, dy1 = 200;
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+    speak('Si quires saber tu ubicacion actual gira el celular a la derecha. Si quieres enviar tu ubicacion actual gira el celular a la izquierda');
+   return Scaffold(
+    body: StreamBuilder<GyroscopeEvent>(
+      stream: SensorsPlatform.instance.gyroscopeEvents,
+      builder: (_, snapshot) {
+        if (snapshot.hasData) {
+          dx1 = dx1 + (snapshot.data!.y*10);
+          dy1 = dy1 + (snapshot.data!.x*10);
+          if(dx1.toInt() != dx.toInt() && dy1.toInt() != dy.toInt()){
+            if(dx1.toInt() > (dx.toInt() + 80) && dx1.toInt() != 100){
+            speak('derecha');
+            dx = dx1;
+            return Transform.translate(offset: Offset(dx1, dy1),
+             child: const CircleAvatar(radius: 20,));
+          }else if(dx1.toInt() < (dx.toInt() -  80) && dx1.toInt() != 100){
+            speak('izquierda');
+            dx = dx1;
+            return Transform.translate(offset: Offset(dx1, dy1),
+            child: const CircleAvatar(radius: 20,));
+          }
+          } 
+        }
+        return Transform.translate(offset: Offset(dx1, dy1),
+        child: const CircleAvatar(radius: 20,));
+      },
+    ),
+   );
   }
 }
