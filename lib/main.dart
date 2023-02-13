a087268e06d440dd06879db732fd907884703281
Nahuel Zalazar <zvnahuel63@gmail.com>
2023-02-13 15:29:42 -0400

    merge

--- lib/main.dart
+++ lib/main.dart
@@ -20,13 +20,12 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Ayudame'),
-        ),
-        body: Center(
-          child: MyHomePage(),
-        )
-      ),
+          appBar: AppBar(
+            title: const Text('Ayudame'),
+          ),
+          body: Center(
+            child: MyHomePage(),
+          )),
     );
   }
 }
@@ -35,11 +34,10 @@ class MyHomePage extends StatelessWidget {
   final FlutterTts flutterTts = FlutterTts();
   MyHomePage({Key? key}) : super(key: key);
 
-
   speak(String text) async {
-  await flutterTts.setLanguage("es-ES");
-  await flutterTts.setPitch(1);
-  await flutterTts.speak(text);
+    await flutterTts.setLanguage("es-ES");
+    await flutterTts.setPitch(1);
+    await flutterTts.speak(text);
   }
 
   double dx = 200, dy = 200;
@@ -47,32 +45,43 @@ class MyHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    speak('Si quires saber tu ubicacion actual gira el celular a la derecha. Si quieres enviar tu ubicacion actual gira el celular a la izquierda');
-   return Scaffold(
-    body: StreamBuilder<GyroscopeEvent>(
-      stream: SensorsPlatform.instance.gyroscopeEvents,
-      builder: (_, snapshot) {
-        if (snapshot.hasData) {
-          dx1 = dx1 + (snapshot.data!.y*10);
-          dy1 = dy1 + (snapshot.data!.x*10);
-          if(dx1.toInt() != dx.toInt() && dy1.toInt() != dy.toInt()){
-            if(dx1.toInt() > (dx.toInt() + 80) && dx1.toInt() != 100){
-            speak('derecha');
-            dx = dx1;
-            return Transform.translate(offset: Offset(dx1, dy1),
-             child: const CircleAvatar(radius: 20,));
-          }else if(dx1.toInt() < (dx.toInt() -  80) && dx1.toInt() != 100){
-            speak('izquierda');
-            dx = dx1;
-            return Transform.translate(offset: Offset(dx1, dy1),
-            child: const CircleAvatar(radius: 20,));
+    speak(
+        'Si quires saber tu ubicacion actual gira el celular a la derecha. Si quieres enviar tu ubicacion actual gira el celular a la izquierda');
+    return Scaffold(
+      body: StreamBuilder<GyroscopeEvent>(
+        stream: SensorsPlatform.instance.gyroscopeEvents,
+        builder: (_, snapshot) {
+          if (snapshot.hasData) {
+            dx1 = dx1 + (snapshot.data!.y * 10);
+            dy1 = dy1 + (snapshot.data!.x * 10);
+            if (dx1.toInt() != dx.toInt() && dy1.toInt() != dy.toInt()) {
+              if (dx1.toInt() > (dx.toInt() + 80) && dx1.toInt() != 100) {
+                speak('derecha');
+                dx = dx1;
+                return Transform.translate(
+                    offset: Offset(dx1, dy1),
+                    child: const CircleAvatar(
+                      radius: 20,
+                    ));
+              } else if (dx1.toInt() < (dx.toInt() - 80) &&
+                  dx1.toInt() != 100) {
+                speak('izquierda');
+                dx = dx1;
+                return Transform.translate(
+                    offset: Offset(dx1, dy1),
+                    child: const CircleAvatar(
+                      radius: 20,
+                    ));
+              }
+            }
           }
-          } 
-        }
-        return Transform.translate(offset: Offset(dx1, dy1),
-        child: const CircleAvatar(radius: 20,));
-      },
-    ),
-   );
+          return Transform.translate(
+              offset: Offset(dx1, dy1),
+              child: const CircleAvatar(
+                radius: 20,
+              ));
+        },
+      ),
+    );
   }
 }
