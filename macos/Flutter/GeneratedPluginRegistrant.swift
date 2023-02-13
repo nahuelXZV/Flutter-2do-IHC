91a7bcc8674fca5414aba09f9d0e7dffb1c284ee
Nahuel Zalazar <zvnahuel63@gmail.com>
2023-02-13 15:35:37 -0400

    fixed

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,17 +5,12 @@
 import FlutterMacOS
 import Foundation
 
-<<<<<<< HEAD
 import flutter_tts
-
-func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  FlutterTtsPlugin.register(with: registry.registrar(forPlugin: "FlutterTtsPlugin"))
-=======
 import path_provider_foundation
 import text_to_speech_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FlutterTtsPlugin.register(with: registry.registrar(forPlugin: "FlutterTtsPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   TextToSpeechMacOsPlugin.register(with: registry.registrar(forPlugin: "TextToSpeechMacOsPlugin"))
->>>>>>> nahuel
 }
